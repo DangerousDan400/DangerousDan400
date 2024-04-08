8c4d8ac5bfbad88ab02ce4e36871719ad4303064
DangerousDan400 <166445593+DangerousDan400@users.noreply.github.com>
2024-04-08 22:32:18 +0200

    Rude computer quiz. Partake at own risk!!
    Foul language warning, if you are a sensitive person i do not recommend you partake in this quiz.

---
+++ quiz.py
@@ -0,0 +1,28 @@
+print("Welcome to my computer quiz!")
+playing = input ("Do you want to play? ")
+if playing != "yes":
+    quit()
+print ("Okay! Let's play :)")
+
+awnser = input ("What does CPU stand for? ")
+if awnser == "central processing unit":
+    print("correct!")
+else:
+    print ("Wrong! i always knew you were a fuckwit")
+
+awnser = input ("What does PSU stand for? ")
+if awnser == "power supply":
+    print ("Correct!")
+else:
+    print ("worng! I always knew you were a fuckwit")
+awnser = input ("What does GPU stand for? ")
+if awnser == "graphical processing unit":
+    print("Correct!")
+else:
+    print("Worng! dumb fuck you will never make it in the trade!!")
+awnser = input ("What does RAM stand for? ")
+if awnser == "ready access memmory":
+    print ("Correct!")
+else:
+    print("Wrong!!!")
+
